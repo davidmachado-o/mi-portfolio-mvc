05db6b6137d0f7f5360b484d31a639ac56ac6bd9
davemachado-o <davidmachadodj@gmail.com>
2022-05-10 16:05:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require("path");
+
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+const rutasMain = require('./routers/main.js');
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+}
+);
+
+app.use('/', rutasMain);
---
+++ routers/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const path = require("path");
+
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../views/home.html"))
+}
+);
+
+router.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../views/about.html"))
+}
+);
+
+module.exports = router;
+
