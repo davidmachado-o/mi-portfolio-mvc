b04f4d1e2508ca6eeebdaef2237f32b92166c884
davemachado-o <davidmachadodj@gmail.com>
2022-05-10 16:26:38 -0300

    mod + comments app.js

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// PUBLIC FOLDER
 const path = require("path");
-
 const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
-const rutasMain = require('./routers/main.js');
-
+// SERVER RUNNING
 app.listen(port, () => {
     console.log(`Running on port ${port}!`);
 }
 );
+
+// RUTAS MAIN
+
+const rutasMain = require('./routers/main.js');
 
 app.use('/', rutasMain);
